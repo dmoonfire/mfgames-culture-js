3e0ac520571182e64b96eafba495b984d1f169a6
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2015-10-27 23:04:46 -0500

    Yeah, no idea.

--- lib/init.ts
+++ lib/init.ts
@@ -1,7 +1,52 @@
+/// <reference path="../typings/node/node.d.ts"/>
+import * as fs from "fs";
+import * as path from "path";
+
 module MfGamesCulture {
+	export interface CalendarCalculationData {
+		div?: number;
+		ref: string;
+		mod?: number;
+	}
+	export interface CalendarLengthData {
+		count: number;
+		julian?: number;
+		choose?: Array<CalendarLengthData>;
+		ref?: string;
+	}
+	export interface CalendarCycleData {
+		id: string;
+		cycles: Array<CalendarCycleData>;
+		calculate?: CalendarCalculationData;
+		lengths?: Array<CalendarLengthData>;
+	}
+
+	/**
+	 * The metadata and top-level information for calendar data.
+	 */
+	export interface CalendarData {
+		version: number;
+		id: string;
+		julian?: number;
+		cycles?: Array<CalendarCycleData>;
+	}
+
     export class Calendar {
-        constructor() {
-            console.log("test 2");
+        constructor(data: CalendarData) {
+            this._data = data;
         }
+
+		private _data: CalendarData;
     }
+
+	export class GregorianCalendar extends MfGamesCulture.Calendar {
+		constructor() {
+			// Load the Gregorian data from the disk.
+			var filename = path.join(__dirname, "gregorian.json");
+			var data: MfGamesCulture.CalendarData = fs.readFileSync(filename).toJSON();
+
+			// Pass the loaded data to the calendar.
+			super(data);
+		}
+	}
 }
---
+++ spec/gregorian-spec.ts
@@ -0,0 +1,12 @@
+/// <reference path="../typings/jasmine/jasmine.d.ts"/>
+/// <reference path="../lib/init.ts"/>
+/// <reference path="../lib/init.ts"/>
+
+import * as calendar from "../lib/init";
+
+describe("gregorian", function() {
+    it("can create", function () {
+        console.log(calendar);
+        expect("test").toEqual("test");
+    });
+});
