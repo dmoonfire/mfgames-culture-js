08ad8c1cc1992193ec37d7e752c0e868629783a8
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2015-10-28 20:27:16 -0500

    Working on unit tests to try handling parsing.

--- src/init.ts
+++ src/init.ts
@@ -8,17 +8,17 @@ export interface CalendarLengthData {
     julian?: number;
     single?: Array<CalendarLengthData>;
     ref?: string;
-	operation?: string;
-	value?: number;
+    operation?: string;
+    value?: number;
 }
 export interface CalendarCycleData {
     id: string;
-	type: string;
+    type: string;
     cycles: Array<CalendarCycleData>;
-	lengths?: Array<CalendarLengthData>;
-	ref?: string;
-	value?: number;
-	operation?: string;
+    lengths?: Array<CalendarLengthData>;
+    ref?: string;
+    value?: number;
+    operation?: string;
 }
 
 /**
@@ -56,90 +56,131 @@ export class Calendar {
     }
 
     private calculateCycle(cycle: CalendarCycleData, julianDate: number, point: any): void {
-		// Figure out what to do based on the type of the cycle.
-		switch (cycle.type)
-		{
-			case "repeat":
-				this.calculateRepeatCycle(cycle, julianDate, point);
-				break;
+        // Figure out what to do based on the type of the cycle.
+        switch (cycle.type) {
+            case "repeat":
+                this.calculateRepeatCycle(cycle, julianDate, point);
+                break;
 
-			case "calculate":
-				this.calculateCalculateCycle(cycle, julianDate, point);
-				break;
+            case "calculate":
+                this.calculateCalculateCycle(cycle, julianDate, point);
+                break;
 
-			default: throw "Cannot handle cycle type of " + cycle.type + ".";
-		}
+            case "sequence":
+                this.calculateSequenceCycle(cycle, julianDate, point);
+                break;
+
+            default: throw "Cannot handle cycle type of " + cycle.type + ".";
+        }
     }
 
-	private calculateCalculateCycle(cycle: CalendarCycleData, julianDate: number, point: any): void {
-		var ref = cycle.ref;
-		var index = point[ref];
-		var value = cycle.value;
+    private calculateCalculateCycle(cycle: CalendarCycleData, julianDate: number, point: any): void {
+        var ref = cycle.ref;
+        var index = point[ref];
+        var value = cycle.value;
 
-		switch (cycle.operation)
-		{
-			case "mod": point[cycle.id] = index % value; break;
-			case "div": point[cycle.id] = Math.floor(index / value); break;
-		}
-	}
+        switch (cycle.operation) {
+            case "mod": point[cycle.id] = index % value; break;
+            case "div": point[cycle.id] = Math.floor(index / value); break;
+        }
 
-	private calculateRepeatCycle(cycle: CalendarCycleData, julianDate: number, point: any): void {
-		// Start with the zero index.
-		point[cycle.id] = 0;
+        // If we have additional cycles, we want to calculate them recursively.
+        if (cycle.cycles) {
+            for (var child of cycle.cycles) {
+                this.calculateCycle(child, julianDate, point);
+            }
+        }
+    }
 
-		// Loop through the various lengths until we encounter a length that
-		// exceeds the remaining Julian Date.
-		var next = 0;
+    private calculateRepeatCycle(cycle: CalendarCycleData, julianDate: number, point: any): void {
+        // Start with the zero index.
+        point[cycle.id] = 0;
 
-		while (julianDate > 0)
-		{
-			console.log(cycle.id, julianDate);
-			// Calculate the length of the next cycle by finding the next one.
-			var found = false;
+        // Loop through the various lengths until we encounter a length that
+        // exceeds the remaining Julian Date.
+        var next = 0;
 
-			for (var length of cycle.lengths)
-			{
-				// Calculate the length of this length. If this is less than or
-				// equal to the Julian Date, we need to keep it.
-				next = this.calculateLength(length, point);
+        while (julianDate >= 0) {
+            // Calculate the length of the next cycle by finding the next one.
+            var found = false;
 
-				if (next > 0 && next <= julianDate) {
-					point[cycle.id] += length.count;
-					julianDate -= next;
-					found = true;
-					break;
-				}
-			}
+            for (var length of cycle.lengths) {
+                // Calculate the length of this length. If this is less than or
+                // equal to the Julian Date, we need to keep it.
+                next = this.calculateLength(length, point);
 
-			// If we fall through, then there is something wrong so break out.
-			if (!found) { break; }
-		}
+                if (next <= 0) { continue; }
 
-		// If we have additional cycles, we want to calculate them recursively.
-		if (cycle.cycles) {
-			for (var child of cycle.cycles) {
-				this.calculateCycle(child, julianDate, point);
-			}
-		}
-	}
+                if (next <= julianDate) {
+                    point[cycle.id] += length.count;
+                    julianDate -= next;
+                    found = true;
+                    break;
+                }
+            }
 
-	private calculateLength(length: CalendarLengthData, point: any): number {
-		// If we have an operation, then we need to calculate this. If the
-		// operation doesn't match, then return 0 to skip the cycle.
-		if (length.operation)
-		{
-			var ref = length.ref;
-			var index = point[ref];
-			var value = length.value;
+            // If we fall through, then there is something wrong so break out.
+            if (!found) { break; }
+        }
 
-			switch (length.operation)
-			{
-				case "mod": if (index % value != 0) { return 0; }
-				case "div": if (Math.floor(index / value) != 0) { return 0; }
-			}
-		}
+        // If we have additional cycles, we want to calculate them recursively.
+        if (cycle.cycles) {
+            for (var child of cycle.cycles) {
+                this.calculateCycle(child, julianDate, point);
+            }
+        }
+    }
 
-		// We have a valid value, so return the results.
-		return length.julian;
-	}
+    private calculateSequenceCycle(cycle: CalendarCycleData, julianDate: number, point: any): void {
+        // Start with the zero index.
+        point[cycle.id] = 0;
+
+        // Loop through the sequences until we exceed our limit.
+        var found = false;
+
+        for (var length of cycle.lengths) {
+            // Calculate the length of this length. If this is less than or
+            // equal to the Julian Date, we need to keep it and move to the next.
+            var next = this.calculateLength(length, point);
+
+            if (next <= 0 || next > julianDate) { break; }
+
+            // Adjust the points and move to the next.
+            point[cycle.id]++;
+            julianDate -= next;
+
+			// If we hit zero, we're done.
+			if (julianDate <= 0) { break; }
+        }
+
+        // If we have additional cycles, we want to calculate them recursively.
+        if (cycle.cycles) {
+            for (var child of cycle.cycles) {
+                this.calculateCycle(child, julianDate, point);
+            }
+        }
+    }
+
+    private calculateLength(length: CalendarLengthData, point: any): number {
+        // If we have an operation, then we need to calculate this. If the
+        // operation doesn't match, then return 0 to skip the cycle.
+        if (length.operation) {
+            var ref = length.ref;
+            var index = point[ref];
+            var value = length.value;
+
+            switch (length.operation) {
+                case "mod":
+                    if (index % value != 0) { return 0; }
+                    break;
+
+                case "div":
+                    if (Math.floor(index / value) != 0) { return 0; }
+                    break;
+            }
+        }
+
+        // We have a valid value, so return the results.
+        return length.julian;
+    }
 }
--- src/spec/gregorian-spec.ts
+++ src/spec/gregorian-spec.ts
@@ -1,19 +1,105 @@
 /// <reference path="../../typings/jasmine/jasmine.d.ts"/>
+/// <reference path="../../typings/julian/julian.d.ts"/>
 import { Calendar } from "../init";
 import { GregorianCalendar } from "../gregorian";
 
 // Set up the Julian date
-function getJulian(date: Date)
-{
-    return (date.getTime() / 86400000) - (date.getTimezoneOffset()/1440) + 2440587.5;
+function getJulian(year: number, month: number, day: number): number {
+    var a = Math.floor((14 - month) / 12);
+    var y = year + 4800 - a;
+    var m = month + 12 * a - 3;
+    var jdn = day + Math.floor((153 * m + 2) / 5) + 365 * y + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(y / 400) - 32045;
+    return jdn - 0.5;
+    //var date = new Date(Date.UTC(year, month, day));
+    //var time = date.getTime();
+    //var jd = (time / 86400000) + 2440587.5 - 31;
+    //return jd;
 }
 
-describe("gregorian", function() {
-    it("verify 1583-01-01", function() {
+describe("julian dates", function() {
+    it("can get for 1583-01-01", function() {
         var cal = new GregorianCalendar();
-        var julian = getJulian(new Date(1583, 1, 1));
+        var julian = getJulian(1583, 1, 1);
+        expect(julian).toEqual(2299238.5);
+    });
+
+    it("can get for 1801-01-01", function() {
+        var cal = new GregorianCalendar();
+        var julian = getJulian(1801, 1, 1);
+        expect(julian).toEqual(2378861.5);
+    });
+
+    it("can get for 2001-01-01", function() {
+        var cal = new GregorianCalendar();
+        var julian = getJulian(2001, 1, 1);
+        expect(julian).toEqual(2451910.5);
+    });
+
+    it("can get for 2001-02-01", function() {
+        var cal = new GregorianCalendar();
+        var julian = getJulian(2001, 2, 1);
+        expect(julian).toEqual(2451941.5);
+    });
+
+    it("can get for 2001-03-01", function() {
+        var cal = new GregorianCalendar();
+        var julian = getJulian(2001, 3, 1);
+        expect(julian).toEqual(2451969.5);
+    });
+});
+
+describe("gregorian calendar", function() {
+    it("can get components for 2001-01-01", function() {
+        var cal = new GregorianCalendar();
+        var julian = getJulian(2001, 1, 1);
         var point = cal.getPoint(julian);
-        console.log(julian, point);
-        expect("test").toEqual("test");
+        expect(point).toEqual({
+            year: 2001,
+            century: 20,
+            millenniumCentury: 0,
+            decade: 200,
+            centuryYear: 0,
+            decadeYear: 1,
+            millennium: 2,
+            yearDay: 0,
+            yearMonth: 0,
+            monthDay: 0
+        });
+    });
+
+    it("can get components for 2001-01-02", function() {
+        var cal = new GregorianCalendar();
+        var julian = getJulian(2001, 1, 2);
+        var point = cal.getPoint(julian);
+        expect(point).toEqual({
+            year: 2001,
+            century: 20,
+            millenniumCentury: 0,
+            decade: 200,
+            centuryYear: 0,
+            decadeYear: 1,
+            millennium: 2,
+            yearDay: 1,
+            yearMonth: 0,
+            monthDay: 1
+        });
+    });
+
+    it("can get components for 2001-02-01", function() {
+        var cal = new GregorianCalendar();
+        var julian = getJulian(2001, 2, 1);
+        var point = cal.getPoint(julian);
+        expect(point).toEqual({
+            year: 2001,
+            century: 20,
+            millenniumCentury: 0,
+            decade: 200,
+            centuryYear: 0,
+            decadeYear: 1,
+            millennium: 2,
+            yearDay: 31,
+            yearMonth: 1,
+            monthDay: 0
+        });
     });
 });
