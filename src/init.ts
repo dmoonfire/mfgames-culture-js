449b6ed1f0f4af2a48890f8d97f0e6c56a4987a6
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2015-11-03 21:20:20 -0600

    Got the basic test framework working.

--- src/init.ts
+++ src/init.ts
@@ -288,7 +288,36 @@ export class CultureProvider {
 
                 dataPromise.then(
                     function(data) {
-                        resolve(new Culture(data));
+                        // Create the base culture from the data.
+                        var culture = new Culture(data);
+
+                        // We need to load all the calendars for this culture.
+                        // Since these are loaded via promises, we need to
+                        // resolve all of them.
+                        var calendarPromises = Array<Promise<Calendar>>();
+
+                        if (data.temporal && data.temporal.calendars) {
+                            for (var calendarId of data.temporal.calendars) {
+                                var calendarPromise = that.getCalendarPromise(calendarId);
+                                calendarPromises.push(calendarPromise);
+                            }
+                        }
+
+                        var allCalendarPromises = Promise.all(calendarPromises);
+
+                        allCalendarPromises.then(
+                            function(promises) {
+                                // If we have no promises, nothing to worry about.
+                                if (promises.length == 0) {}
+                                else if (promises.length == 1) {
+                                    culture.calendar = promises[0];
+                                }
+                                else { throw new Error("Cannot assign multiple calendars."); }
+
+                                // Resolve the culture.
+                                resolve(culture);
+                            }
+                        )
                     },
                     function(dataError) {
                         error(dataError);
--- src/spec/format-gregorian-spec.ts
+++ src/spec/format-gregorian-spec.ts
@@ -7,18 +7,23 @@ describe("gregorian format", function() {
     it("can format 2001-01-01 as MM/YY/DDDD", function(done) {
         getCulture("en-US").then(
             function(culture: Culture) {
-                console.log("got a culture", culture);
-                var julian = getJulian(2001, 1, 1);
-                var calendar = culture.calendar;
-                expect(calendar).toBeDefined();
+                try {
+                    var julian = getJulian(2001, 1, 1);
+                    var calendar = culture.calendar;
+                    expect(calendar).toBeDefined();
 
-                var instant = calendar.getInstant(julian);
-                var formattedInstant = culture.formatInstant(instant, "MM/DD/YYYY");
-                expect(formattedInstant).toEqual(instant, "01/01/2001");
-                done();
+                    var instant = calendar.getInstant(julian);
+                    var formattedInstant = culture.formatInstant(instant, "MM/DD/YYYY");
+                    console.log("instant", formattedInstant);
+                    expect(formattedInstant).toEqual(instant, "01/01/2001");
+                } catch(err) {
+                    fail(err);
+                } finally {
+                    done();
+                }
             },
             function(error) {
-                throw error;
+                fail("There was an error while running");
             });
     });
 });
