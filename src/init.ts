522703209650cfc921357652ff208266c4e2d832
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2015-11-04 13:24:41 -0600

    Getting formatting of points working.

--- src/init.ts
+++ src/init.ts
@@ -30,20 +30,24 @@ export interface CalendarData {
     julian?: number;
     cycles?: Array<CalendarCycleData>;
 }
-export interface CultureTemporalFormatData {
+export interface CultureTemporalFormatElementData {
     ref?: string;
     constant?: string;
     digits?: number;
     offset?: number;
+    prefix?: string;
+    suffix?: string;
+    lookup?: boolean;
 }
 export interface CultureTemporalData {
     calendars: Array<string>;
-    formats: Array<CultureTemporalFormatData>;
+    formats: { [id: string]: Array<CultureTemporalFormatElementData> };
 }
 export interface CultureData {
     id: string;
     version: number;
     temporal?: CultureTemporalData;
+    lookups: { [id: string]: string };
 }
 export interface CultureDataProvider {
     getCalendarData(id: string): Promise<CalendarData>;
@@ -242,14 +246,66 @@ export class Calendar {
 
 export class Culture {
     constructor(data: CultureData) {
+        if (!data) { throw new Error("Cannot create a culture without data.") }
         this._data = data;
     }
 
     private _data: CultureData;
     public calendar: Calendar;
 
-    public formatInstant(instant: any, id: string): string {
-        throw "Cannot format instants";
+    public formatInstant(instant: any, formatId: string): string {
+        // First make sure this is a known format for this culture.
+        var elements = this._data.temporal.formats[formatId];
+        if (!elements) { throw new Error("Unknown format ID: " + formatId + "."); }
+
+        // We have the format, so build up a string that contents the elements.
+        var buffer = "";
+
+        for (var elem of elements) {
+            // If we are a constant, we can just add it.
+            if (elem.constant) {
+                buffer += elem.constant;
+                continue;
+            }
+
+            // If we have a reference, then format the cycle.
+            if (elem.ref) {
+                // Get the cycle index.
+                var cycleIndex = instant[elem.ref];
+
+                if (cycleIndex === undefined) {
+                    throw new Error("Cannot find cycle " + elem.ref + " in " + instant + ".");
+                }
+
+                // If we have an offset, modify to get the right value.
+                if (elem.offset) { cycleIndex += elem.offset; }
+
+                // First handle any zero-padding.
+                var value: string = cycleIndex.toString();
+
+                if (elem.digits) {
+                    while (value.length < elem.digits) {
+                        value = "0" + value;
+                    }
+                }
+
+                // If we have a prefix or suffix, add them.
+                if (elem.prefix) { value = elem.prefix + value; }
+                if (elem.suffix) { value = elem.suffix + value; }
+
+                // If we have a lookup code, then use the resulting value as
+                // a lookup.
+                if (elem.lookup) {
+                    value = this._data.lookups[value];
+                }
+
+                // Add the formatted value to the buffer.
+                buffer += value;
+            }
+        }
+
+        // Return the resulting text.
+        return buffer;
     }
 }
 
@@ -308,7 +364,7 @@ export class CultureProvider {
                         allCalendarPromises.then(
                             function(promises) {
                                 // If we have no promises, nothing to worry about.
-                                if (promises.length == 0) {}
+                                if (promises.length == 0) { }
                                 else if (promises.length == 1) {
                                     culture.calendar = promises[0];
                                 }
@@ -317,7 +373,7 @@ export class CultureProvider {
                                 // Resolve the culture.
                                 resolve(culture);
                             }
-                        )
+                            )
                     },
                     function(dataError) {
                         error(dataError);
--- src/spec/format-gregorian-spec.ts
+++ src/spec/format-gregorian-spec.ts
@@ -4,7 +4,25 @@ import { Calendar, Culture } from "../init";
 import { getJulian, getCalendar, getCulture } from "./helper";
 
 describe("gregorian format", function() {
-    it("can format 2001-01-01 as MM/YY/DDDD", function(done) {
+    it("can't format 2001-01-01 as INVALID", function(done) {
+        getCulture("en-US").then(
+            function(culture: Culture) {
+                try {
+                    var julian = getJulian(2001, 1, 1);
+                    var calendar = culture.calendar;
+                    var instant = calendar.getInstant(julian);
+                    var formattedInstant = culture.formatInstant(instant, "INVALID");
+                    fail("Incorrectly formatted a date as INVALID.")
+                } finally {
+                    done();
+                }
+            },
+            function(error) {
+                fail("There was an error while running");
+            });
+    });
+
+    it("can format 2001-01-01 as MM/DD/YYYY", function(done) {
         getCulture("en-US").then(
             function(culture: Culture) {
                 try {
@@ -14,8 +32,29 @@ describe("gregorian format", function() {
 
                     var instant = calendar.getInstant(julian);
                     var formattedInstant = culture.formatInstant(instant, "MM/DD/YYYY");
-                    console.log("instant", formattedInstant);
-                    expect(formattedInstant).toEqual(instant, "01/01/2001");
+                    expect(formattedInstant).toEqual("01/01/2001");
+                } catch(err) {
+                    fail(err);
+                } finally {
+                    done();
+                }
+            },
+            function(error) {
+                fail("There was an error while running");
+            });
+    });
+
+    it("can format 2001-01-01 as MMM DD, YY", function(done) {
+        getCulture("en-US").then(
+            function(culture: Culture) {
+                try {
+                    var julian = getJulian(2001, 1, 1);
+                    var calendar = culture.calendar;
+                    expect(calendar).toBeDefined();
+
+                    var instant = calendar.getInstant(julian);
+                    var formattedInstant = culture.formatInstant(instant, "MMM DD, YY");
+                    expect(formattedInstant).toEqual("Jan 01, 01");
                 } catch(err) {
                     fail(err);
                 } finally {
